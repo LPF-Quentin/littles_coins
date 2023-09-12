4cadc83e6bf787bce3d011b40f9161b208bef511
LPF-Quentin <quentin-rousseau@orange.fr>
2023-09-12 20:50:30 +0200

    recup

---
+++ src/Components/Body.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import '../Layouts/Body.css'
+
+const Body = () => {
+    return ( 
+        <div className="body">
+            szfdfd
+        </div>
+     );
+}
+ 
+export default Body;
